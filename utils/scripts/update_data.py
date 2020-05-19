d2c79f2e7aa72338c4ecac2a02aecb3108cd9b12
zurmad <pablo.diazv@pucp.edu.pe>
2020-05-19 13:50:27 -0500

    2020-05-18.csv BR,CO,CR,EC,PE
    Former-commit-id: 4031847905c7fac14da907c38e557159589eeb62
    Former-commit-id: 3350320c91c650a90601930c6f4d1ccb12cb2c48

--- utils/scripts/update_data.py
+++ utils/scripts/update_data.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
     today = datetime.datetime.today()
     date_list_csv, date_list = generate_list_dates(path_dsrp)
     # HERE YOU DEFINE THE RANGE OF DATES TO UPDATE
-    list_date_list = date_list[1:-75]
+    list_date_list = date_list[1:5]
 
     load_all_data_temporal(list_date_list)
 
